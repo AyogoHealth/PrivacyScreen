df0579c59506bbf416bcc24270e462c5a491b99b
Darryl Pogue <darryl@ayogo.com>
2016-10-05 15:41:33 -0700

    Rewrite the iOS Privacy Screen plugin

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -1,26 +1,16 @@
-/**
- * PrivacyScreenPlugin.java Cordova Plugin Implementation
- * Created by Tommy-Carlos Williams on 18/07/14.
+/*!
  * Copyright (c) 2014 Tommy-Carlos Williams. All rights reserved.
- * MIT Licensed
+ * Copyright 2016 Ayogo Health Inc.
  */
-package org.devgeeks.privacyscreen;
+package com.ayogo.privacyscreen;
 
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
 import android.app.Activity;
 import android.view.Window;
 import android.view.WindowManager;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.os.Bundle;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
@@ -32,9 +22,9 @@ public class PrivacyScreenPlugin extends CordovaPlugin {
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
-    Activity activity = this.cordova.getActivity();
 
     if (preferences.getBoolean("NoAppScreenshots", true)) {
+      Activity activity = this.cordova.getActivity();
       activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
     }
   }
